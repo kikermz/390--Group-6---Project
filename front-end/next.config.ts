ed7867023915fa984e40375f8cf3932e96debdec
kikermz <134441849+kikermz@users.noreply.github.com>
2025-04-06 15:10:19 -0500

    Made sure the api can display all domains

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -3,12 +3,16 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['*', 'localhost', 's.yimg.com', 'npr.brightspotcdn.com', 'media-cldnry.s-nbcnews.com', 'www.washingtonpost.com', 'a4.espncdn.com'
-      , 'dims.apnews.com', 'www.ft.com', 'images.axios.com', 'www.cnbc.com', 'www.bbc.co.uk', 'www.theguardian.com', 'www.nytimes.com', 'www.reuters.com'
-      , 'www.foxnews.com', 'www.wsj.com', 'www.bloomberg.com', 'www.politico.com', 'www.nbcnews.com', 'www.cnn.com', 'www.usatoday.com'
-      , 'www.latimes.com', 'www.forbes.com', 'www.huffpost.com', 'www.vox.com', 'www.businessinsider.com', 'www.vice.com', 'www.salon.com'
-      , 'www.washingtonexaminer.com', 'www.npr.org', 'www.pbs.org', 'www.c-span.org', 'www.c-span.org', 'www.c-span.org', 'www.c-span.org'
-    ], 
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Allows all domains
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost', // Explicitly allow localhost
+      },
+    ],
   },
 };
 
